882cb85d08ca343f293020343cb0e85556a132ac
bananayosostene01~ <sbananayo98@gmail.com>
2023-01-06 15:53:09 +0200

    second new commit

--- js/script.js
+++ js/script.js
@@ -15,3 +15,37 @@ btn.addEventListener("click", () => {
 document.querySelector(".close").addEventListener("click", () => {
   document.querySelector(".modal").style.display = "none";
 });
+$(function () {
+  $(".chart").easyPieChart({
+    size: 160,
+    barColor: "#36e617",
+    scaleLength: 0,
+    lineWidth: 15,
+    trackColor: "#525151",
+    lineCap: "circle",
+    animate: 2000,
+  })});
+function myFunction() {
+  // Get the value of the input field with id="numb"
+  var x = document.getElementById("name").value;
+  // If x is Not a Number or less than one or greater than 10
+  var text;
+  if (x =="") {
+    text = "Input not valid";
+  } else {
+     text = " ";
+  }
+  document.getElementById("demo").innerHTML = text;
+}
+function ValidateEmail(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.value.match(mailformat)) {
+    alert("Valid email address!");
+    document.form1.email.focus();
+    return true;
+  } else {
+    alert("You have entered an invalid email address!");
+    document.form1.email.focus();
+    return false;
+  }
+}
